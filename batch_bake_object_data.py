c1efca409e2b3d7e1c915edb0a4f9edc98392588
florianfelix <florianfelixmeyer@gmail.com>
2016-06-19 04:17:00 +0200

    further simplification of aov passes

--- batch_bake_object_data.py
+++ batch_bake_object_data.py
@@ -9,24 +9,86 @@ from bpy.props import *
 from bpy.types import PropertyGroup
 
 size_items = [('265','265',''),
-              ('512','512',''),
-              ('1024','1024',''),
-              ('2048','2048',''),
-              ('4096','4096',''),
-              ('CUSTOM','Custom',''),
-             ]
+    ('512','512',''),
+    ('1024','1024',''),
+    ('2048','2048',''),
+    ('4096','4096',''),
+    ('CUSTOM','Custom',''),
+    ]
 normal_space_items = [
-             ('TANGENT', 'Tangent', ''),
-             ('OBJECT', 'Object', ''),
-             ]
+    ('TANGENT', 'Tangent', ''),
+    ('OBJECT', 'Object', ''),
+    ]
 swizzle_items = [
-             ('POS_X', '+X', ''),
-             ('POS_Y', '+Y', ''),
-             ('POS_Z', '+Z', ''),
-             ('NEG_X', '-X', ''),
-             ('NEG_Y', '-Y', ''),
-             ('NEG_Z', '-Z', ''),
-             ]
+    ('POS_X', '+X', ''),
+    ('POS_Y', '+Y', ''),
+    ('POS_Z', '+Z', ''),
+    ('NEG_X', '-X', ''),
+    ('NEG_Y', '-Y', ''),
+    ('NEG_Z', '-Z', ''),
+    ]
+
+class AOV(PropertyGroup):
+    #for all passes
+    use = BoolProperty(name='use',
+        default=False)
+    dimensions = EnumProperty(
+        items=size_items,
+        )
+    dimensions_custom = IntVectorProperty(
+        name='Dimensions',
+        size=2,
+        default=(265, 265)
+        )
+
+    #for select passes
+    use_pass_direct = BoolProperty(
+        name='Direct',
+        default=True)
+    use_pass_indirect = BoolProperty(
+        name='Indirect',
+        default=True)
+    use_pass_color = BoolProperty(
+        name='Color',
+        default=True)
+
+    #for combined pass
+    use_pass_transmission = BoolProperty(
+        name='Transmission',
+        default=True)
+    use_pass_ao = BoolProperty(
+        name='AO',
+        default=True)
+    use_pass_emit = BoolProperty(
+        name='Emit',
+        default=True)
+    use_pass_subsurface = BoolProperty(
+        name='Subsurface',
+        default=True)
+    use_pass_diffuse = BoolProperty(
+        name='Diffuse',
+        default=True)
+    use_pass_glossy = BoolProperty(
+        name='Glossy',
+        default=True)
+
+    #for normal pass
+    normal_space = EnumProperty(
+        name='Space:',
+        items=normal_space_items,
+        )
+    normal_r = EnumProperty(
+        items=swizzle_items,
+        default='POS_X',
+        )
+    normal_g = EnumProperty(
+        items=swizzle_items,
+        default='POS_Y',
+        )
+    normal_b = EnumProperty(
+        items=swizzle_items,
+        default='POS_Z',
+        )
 
 class BBake_Object_Settings(PropertyGroup):
     use = BoolProperty(
@@ -67,245 +129,38 @@ class BBake_Object_Settings(PropertyGroup):
         description='Name of object to use as cage for raycasting.',
         )
 
-class AOV_Diffuse(PropertyGroup):
-    diffuse_pass = BoolProperty(
-        name='Diffuse',
-        default=False,
-        )
-    diffuse_use_pass_direct = BoolProperty(
-        name='Direct',
-        default=True)
-    diffuse_use_pass_indirect = BoolProperty(
-        name='Indirect',
-        default=True)
-    diffuse_use_pass_color = BoolProperty(
-        name='Color',
-        default=True)
+class AOV_Diffuse(AOV):
+    pass
 
+class AOV_Glossy(AOV):
+    pass
 
-    diffsize = EnumProperty(
-        items=size_items,
-        )
-    diffsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
+class AOV_Transmission(AOV):
+    pass
 
-class AOV_Glossy(PropertyGroup):
-    glossy_pass = BoolProperty(
-        name='Glossy',
-        default=False,
-        )
+class AOV_Subsurface(AOV):
+    pass
 
-    glossy_use_pass_direct = BoolProperty(
-        name='Direct',
-        default=True)
-    glossy_use_pass_indirect = BoolProperty(
-        name='Indirect',
-        default=True)
-    glossy_use_pass_color = BoolProperty(
-        name='Color',
-        default=True)
+class AOV_Normal(AOV):
+    pass
 
-    specsize = EnumProperty(
-        items=size_items,
-        )
-    specsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
+class AOV_AO(AOV):
+    pass
 
-class AOV_Transmission(PropertyGroup):
-    transmission_pass = BoolProperty(
-        name='Transmission',
-        default=False,
-        )
+class AOV_Combined(AOV):
+    pass
 
-    transmission_use_pass_direct = BoolProperty(
-        name='Direct',
-        default=True)
-    transmission_use_pass_indirect = BoolProperty(
-        name='Indirect',
-        default=True)
-    transmission_use_pass_color = BoolProperty(
-        name='Color',
-        default=True)
+class AOV_Shadow(AOV):
+    pass
 
-    transmissionsize = EnumProperty(
-        items=size_items,
-        )
-    transmissionsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
+class AOV_Emit(AOV):
+    pass
 
+class AOV_UV(AOV):
+    pass
 
-class AOV_Subsurface(PropertyGroup):
-    subsurface_pass = BoolProperty(
-        name='Subsurface',
-        default=False,
-        )
-
-    subsurface_use_pass_direct = BoolProperty(
-        name='Direct',
-        default=True)
-    subsurface_use_pass_indirect = BoolProperty(
-        name='Indirect',
-        default=True)
-    subsurface_use_pass_color = BoolProperty(
-        name='Color',
-        default=True)
-
-    subsurfacesize = EnumProperty(
-        items=size_items,
-        )
-    subsurfacesize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
-
-class AOV_Normal(PropertyGroup):
-    normal = BoolProperty(
-        name='Normal',
-        default=False,
-        )
-    normalsize = EnumProperty(
-        items=size_items,
-        )
-    normalsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
-    normal_space = EnumProperty(
-        name='Space:',
-        items=normal_space_items,
-        )
-    normal_r = EnumProperty(
-        items=swizzle_items,
-        default='POS_X',
-        )
-    normal_g = EnumProperty(
-        items=swizzle_items,
-        default='POS_Y',
-        )
-    normal_b = EnumProperty(
-        items=swizzle_items,
-        default='POS_Z',
-        )
-
-class AOV_AO(PropertyGroup):
-    ao = BoolProperty(
-        name='AO',
-        default=False,
-        )
-    aosize = EnumProperty(
-        items=size_items,
-        )
-    aosize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
-
-class AOV_Combined(PropertyGroup):
-    combined = BoolProperty(
-        name='Combined',
-        default=False,
-        )
-    combined_use_pass_direct = BoolProperty(
-        name='Direct',
-        default=True)
-    combined_use_pass_indirect = BoolProperty(
-        name='Indirect',
-        default=True)
-    combined_use_pass_transmission = BoolProperty(
-        name='Transmission',
-        default=True)
-    combined_use_pass_ao = BoolProperty(
-        name='AO',
-        default=True)
-    combined_use_pass_emit = BoolProperty(
-        name='Emit',
-        default=True)
-    combined_use_pass_subsurface = BoolProperty(
-        name='Subsurface',
-        default=True)
-    combined_use_pass_diffuse = BoolProperty(
-        name='Diffuse',
-        default=True)
-    combined_use_pass_glossy = BoolProperty(
-        name='Glossy',
-        default=True)
-
-    combinedsize = EnumProperty(
-        items=size_items,
-        )
-    combinedsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
-
-class AOV_Shadow(PropertyGroup):
-    shadow = BoolProperty(
-        name='Shadow',
-        default=False,
-        )
-    shadowsize = EnumProperty(
-        items=size_items,
-        )
-    shadowsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
-
-class AOV_Emit(PropertyGroup):
-    emit = BoolProperty(
-        name='Emit',
-        default=False,
-        )
-    emitsize = EnumProperty(
-        items=size_items,
-        )
-    emitsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
-
-class AOV_UV(PropertyGroup):
-    uv = BoolProperty(
-        name='UV',
-        default=False,
-        )
-    uvsize = EnumProperty(
-        items=size_items,
-        )
-    uvsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
-
-class AOV_Environment(PropertyGroup):
-    env = BoolProperty(
-        name='Environment',
-        default=False,
-        )
-    envsize = EnumProperty(
-        items=size_items,
-        )
-    envsize_custom = IntVectorProperty(
-        name='Dimensions',
-        size=2,
-        default=(265, 265)
-        )
+class AOV_Environment(AOV):
+    pass
 
 ###########################################################################
 class BBake_Object_Data(PropertyGroup):
